740a6b6589b20a797970d7c7d1deda571e4038bc
Kazuki Kubo <kz@assaulter.org>
2022-10-06 16:09:51 +0900

    Initial Commit

---
+++ ChartsSample/ChartsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsSampleApp.swift
+//  ChartsSample
+//
+//  Created by Kazuki Kubo on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct ChartsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
